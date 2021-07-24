f3c952157a2a07b83f22dee4d9b816abd9831ddb
ShariyarAbeer <007abeershariyar@gmail.com>
2021-07-24 15:41:16 +0600

    done but late work

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -8,6 +10,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Position _currentPosition;
+  String _currentAddress;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +26,52 @@ class _HomeState extends State<Home> {
         title: Text("Locaion Tracking"),
       ),
       body: Center(
-        child: Text("hello"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_currentAddress != null) Text(_currentAddress),
+            ElevatedButton(
+              child: Text("Get location"),
+              onPressed: () {
+                _getCurrentLocation();
+                print(_getCurrentLocation());
+              },
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  _getCurrentLocation() {
+    Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high,
+            forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+        _getAddressFromLatLng();
+        print(_getAddressFromLatLng());
+      });
+    }).catchError((e) {
+      print(e);
+      print(_getAddressFromLatLng());
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+
+      Placemark place = placemarks[0];
+
+      setState(() {
+        _currentAddress =
+            "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 }
